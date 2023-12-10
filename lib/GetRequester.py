835616d13151a4bae1213e43d4abde7395f9618c
Mikebrian <mikemunene1173@gmail.com>
2023-12-10 16:03:19 +0300

    Complete assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,16 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.text
+        
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        return json.loads(response_body)
+        
+
+url = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
+get_requester = GetRequester(url)
+json_data = get_requester.load_json()
+print(json_data)
--- lib/testing/get_requester_test.py
+++ lib/testing/get_requester_test.py
@@ -1,14 +1,26 @@
+import json
 from GetRequester import GetRequester
 class GetRequesterTest:
     '''Class {Classname} in {modulename}.py'''
 URL = 'https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json'
-JSON_STRING = b"[\n  {\n    \"name\": \"Daniel\",\n    \"occupation\": \"LG Fridge Salesman\"\n  },\n  {\n    \"name\": \"Joe\",\n    \"occupation\": \"WiFi Fixer\"\n  },\n  {\n    \"name\": \"Avi\",\n    \"occupation\": \"DJ\"\n  },\n  {\n    \"name\": \"Howard\",\n    \"occupation\": \"Mountain Legend\"\n  }\n]\n"
-CONVERTED_DATA = [{ 'name': 'Daniel', 'occupation' : 'LG Fridge Salesman' }, { 'name': 'Joe', 'occupation': 'WiFi Fixer' }, { 'name': 'Avi', 'occupation': 'DJ' }, { 'name': 'Howard', 'occupation': 'Mountain Legend' }]
+JSON_STRING = '[{"name":"Daniel","occupation":"LG Fridge Salesman"},{"name":"Joe","occupation":"WiFi Fixer"},{"name":"Avi","occupation":"DJ"},{"name":"Howard","occupation":"Mountain Legend"}]'
+CONVERTED_DATA = [
+    {'name': 'Daniel', 'occupation': 'LG Fridge Salesman'},
+    {'name': 'Joe', 'occupation': 'WiFi Fixer'},
+    {'name': 'Avi', 'occupation': 'DJ'},
+    {'name': 'Howard', 'occupation': 'Mountain Legend'}
+]
 
 def test_get_response():
         '''get_response_body function returns response.'''
         requester = GetRequester(URL)
-        assert(requester.get_response_body() == JSON_STRING)
+        response_body = requester.get_response_body()
+
+        expected_data = json.loads(JSON_STRING)
+
+        actual_data = json.loads(response_body)
+
+        assert actual_data == expected_data
 
 def test_load_json():
         '''load_json function returns response.'''
